2eb3a5febb8c5866d5c0bbc2f6b2a46fc6737233
Oliver Lindemann <lindemann09@googlemail.com>
2024-07-12 17:50:03 +0200

    zero trigger

--- src/trigger.jl
+++ src/trigger.jl
@@ -5,21 +5,25 @@ struct Trigger
     time_index::AbstractRange
 end
 
-function Trigger(biodat::BiopacData)
+function Trigger(biodat::BiopacData; zero_trigger::Bool=false)
     signal = trigger_bytes(biodat)
     trigger = Int64[]
     ranges = UnitRange{Int64}[]
-    last_tr = 0
+    last_tr = -1
     idx = 0
     for (i, x) in enumerate(signal)
         if x != last_tr
-            if last_tr != 0
+            if last_tr > 0 || (zero_trigger && last_tr != -1)
                 push!(trigger, last_tr)
                 push!(ranges, range(idx, i-1))
             end
             last_tr = x
             idx = i
         end
+    end
+    if last_tr != 0 || zero_trigger
+        push!(trigger, last_tr)
+        push!(ranges, range(idx, length(signal)))
     end
     return Trigger(signal, trigger, ranges, time_index(biodat))
 end
